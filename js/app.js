7288959dc222e2c98fd7019f6a7d55f5aed71876
Nick Mullaney <nick.mullaney@gmail.com>
2022-11-05 20:25:50 +0000

    now with a constructor

--- js/app.js
+++ js/app.js
@@ -1,51 +1,66 @@
 'use strict';
 
-let store1 = {
-  location: `Seattle`,
-  minCustomer: 23,
-  maxCustomer: 65,
-  avgCookieSale: 6.3,
-  //Array of generated sales numbers
-  //Each item should represent an hour of sales
-  hourlySales: [],
-  totalSales: 0
-};
+// let store1 = {
+//   location: `Seattle`,
+//   minCustomer: 23,
+//   maxCustomer: 65,
+//   avgCookieSale: 6.3,
+//   //Array of generated sales numbers
+//   //Each item should represent an hour of sales
+//   hourlySales: [],
+//   totalSales: 0
+// };
 
-let store2 = {
-  location: `Tokyo`,
-  minCustomer: 3,
-  maxCustomer: 24,
-  avgCookieSale: 1.2,
-  hourlySales: [],
-  totalSales: 0
-};
+// let store2 = {
+//   location: `Tokyo`,
+//   minCustomer: 3,
+//   maxCustomer: 24,
+//   avgCookieSale: 1.2,
+//   hourlySales: [],
+//   totalSales: 0
+// };
 
-let store3 = {
-  location: `Dubai`,
-  minCustomer: 11,
-  maxCustomer: 38,
-  avgCookieSale: 3.7,
-  hourlySales: [],
-  totalSales: 0
-};
+// let store3 = {
+//   location: `Dubai`,
+//   minCustomer: 11,
+//   maxCustomer: 38,
+//   avgCookieSale: 3.7,
+//   hourlySales: [],
+//   totalSales: 0
+// };
 
-let store4 = {
-  location: `Paris`,
-  minCustomer: 20,
-  maxCustomer: 38,
-  avgCookieSale: 2.3,
-  hourlySales: [],
-  totalSales: 0
-};
+// let store4 = {
+//   location: `Paris`,
+//   minCustomer: 20,
+//   maxCustomer: 38,
+//   avgCookieSale: 2.3,
+//   hourlySales: [],
+//   totalSales: 0
+// };
 
-let store5 = {
-  location: `Lima`,
-  minCustomer: 2,
-  maxCustomer: 16,
-  avgCookieSale: 4.6,
-  hourlySales: [],
-  totalSales: 0
-};
+// let store5 = {
+//   location: `Lima`,
+//   minCustomer: 2,
+//   maxCustomer: 16,
+//   avgCookieSale: 4.6,
+//   hourlySales: [],
+//   totalSales: 0
+// };
+
+function Store(location, minCustomer, maxCustomer, avgCookieSale, hourlySales, totalSales){
+  this.location = location;
+  this.minCustomer = minCustomer;
+  this.maxCustomer = maxCustomer;
+  this.avgCookieSale = avgCookieSale;
+  this.hourlySales = hourlySales;
+  this.totalSales = totalSales;
+}
+let store1 = new Store(`Seattle`, 23, 65, 6.3, [], 0);
+console.log(store1);
+let store2 = new Store(`Tokyo`, 3, 24, 1.2, [], 0);
+let store3 = new Store(`Dubai`, 1, 38, 3.7, [], 0);
+let store4 = new Store(`Paris`, 20, 38, 2.3, [], 0);
+let store5 = new Store(`Lima`, 2, 16, 4.6, [], 0);
 
 let allStores = [store1, store2, store3, store4, store5];
 let operatingHours = [`6 AM`, `7 AM`, `8 AM`, `9 AM`, `10 AM`, `11 AM`, `12 PM`, `1 PM`, `2 PM`, `3 PM`, `4 PM`, `5 PM`, `6 PM`, `7 PM`];
